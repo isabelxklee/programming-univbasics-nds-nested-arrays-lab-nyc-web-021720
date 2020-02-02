b9fc22c64e8821d1585584b831f397c8a9c9294a
isabel k. lee <hello@isabelklee.co>
2020-02-02 03:59:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -26,7 +26,7 @@ def assembled_matrix
 end
 
 def sorted_matrix
-  
+  array = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
   sorted_array = array.sort
   
   # Using Array literal syntax only, build another nested array that 
